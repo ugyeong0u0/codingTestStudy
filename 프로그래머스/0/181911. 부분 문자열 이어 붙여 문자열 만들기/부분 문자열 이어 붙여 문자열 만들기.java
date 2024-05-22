b4a28cc2a14f27154bf02929f2392ccd4e5170e7
ugyeong <120684605+ugyeong0u0@users.noreply.github.com>
2024-05-22 17:05:17 +0900

    Update 부분 문자열 이어 붙여 문자열 만들기.java

--- 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.java
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.java
@@ -6,23 +6,15 @@ class Solution {
         int len = my_strings.length;
         int rowL = parts.length;
         int colL= parts[0].length;
-
-   
-           
-            int i = 0; 
-        for(int k =0;k <len;k++){
-
-          
-         
             
-              if(i == rowL+1){
+        int i = 0; 
+       
+        for(int k =0;k <len;k++){
+            if(i == rowL+1){
                 break;
             }
-                sb.append(my_strings[k].substring(parts[i][0],parts[i][1]+1)); 
-           i++;
-            
-            
-            
+            sb.append(my_strings[k].substring(parts[i][0],parts[i][1]+1)); 
+            i++;
         }
         answer = sb.toString();
         return answer;
