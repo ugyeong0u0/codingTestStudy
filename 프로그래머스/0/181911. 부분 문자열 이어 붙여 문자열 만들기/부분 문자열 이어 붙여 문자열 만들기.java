2d34e2402cfeef82c987755dd426ed84c209b0e9
ugyeong <120684605+ugyeong0u0@users.noreply.github.com>
2024-05-22 17:07:14 +0900

    Update 부분 문자열 이어 붙여 문자열 만들기.java

--- 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.java
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.java
@@ -8,8 +8,10 @@ class Solution {
         int colL= parts[0].length;
             
         int i = 0; 
-       
-        for(int k =0;k <len;k++){
+
+        /* 문제에서 my_strings와 parts의 인덱스 값이 같이 움직여야하기 때문에 하나의 for문 사용
+       중첩 for문은 하나의 인덱스 당 다른 인덱스 순환일때 사용 */
+        for(int k =0;k <len;k++){ 
             if(i == rowL+1){
                 break;
             }
