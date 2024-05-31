8ef669a08eeeea170fabbb54c8610320e3769643
ugyeong <120684605+ugyeong0u0@users.noreply.github.com>
2024-05-31 12:34:08 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 2.07 ms, Memory: 88 MB -BaekjoonHub

---
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        answer += myString.toUpperCase();
+        
+        
+        return answer;
+    }
+}
