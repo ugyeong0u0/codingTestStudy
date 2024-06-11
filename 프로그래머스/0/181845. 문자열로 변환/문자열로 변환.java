8e1d7f47cb485fe238749f01c16a636015682db4
ugyeong <120684605+ugyeong0u0@users.noreply.github.com>
2024-06-11 15:54:27 +0900

    [level 0] Title: 문자열로 변환, Time: 7.04 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        
+        answer =String.valueOf(n+"");
+        
+        
+        return answer;
+    }
+}
