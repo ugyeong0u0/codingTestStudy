a3079a147c8ef583b58e41c322102fc53542a890
ugyeong <120684605+ugyeong0u0@users.noreply.github.com>
2024-06-13 20:34:02 +0900

    [level 0] Title: 그림 확대, Time: 9.77 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -0,0 +1,32 @@
+import java.util.*;
+// 1. 한 줄 완성
+// 2. 그 한줄 k번 복사 
+class Solution {
+    public String[] solution(String[] picture, int k) {
+        String[] answer = new String[picture.length * k ];
+        List<String> list = new ArrayList<>();
+        
+        for(int i = 0; i< picture.length;i++){
+            String tmp = "";
+            for(int j =0; j< picture[i].length(); j++){ // 한 줄 완성하기
+                for(int l =0 ; l<k;l++){
+                    
+                    tmp+=picture[i].charAt(j);
+                    
+                }
+                   
+            }
+            
+             for(int l =0 ; l<k;l++){
+                    
+                    list.add(tmp); // 한줄씩 k번 넣기
+                    
+                }
+        }
+        
+        answer= list.toArray(new String[list.size()]);
+        
+        
+        return answer;
+    }
+}
