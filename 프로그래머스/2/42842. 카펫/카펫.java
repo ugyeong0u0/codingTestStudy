7a4df15d4a55fd9a948cf9ebbfd174b5f7353b73
ugyeong <120684605+ugyeong0u0@users.noreply.github.com>
2024-06-20 21:35:41 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        
+        int total = brown + yellow;
+        
+        for(int i = 2; i<= Math.sqrt(total); i++){
+            
+            if(total%i ==0){
+                
+                int t1 = total/i;
+                int w = 0;
+                int h = 0;
+                if(t1 >i ){
+
+                    w= t1;
+                    h=i;
+                }else{
+                     w= i;
+                    h=t1;
+                }
+                
+                w-=2;
+                h-=2;
+                
+                if(w*h==yellow){
+
+                    answer[0]=w+2;
+                    answer[1]=h+2;
+                }
+                
+                
+                
+            }
+            
+        }
+        
+        
+        
+        
+        
+        return answer;
+    }
+}
