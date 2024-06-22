b60810cab4df29fa6c6720b1fea93055f050441d
ugyeong <120684605+ugyeong0u0@users.noreply.github.com>
2024-06-22 22:17:39 +0900

    [level 2] Title: 모음 사전, Time: 1.85 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,39 @@
+class Solution {
+    static String Word;
+    static int answer=0;
+    static int count =0; 
+    static char [] dict = {'A', 'E' , 'I', 'O','U'};
+    public int solution(String word) {
+        Word = word;
+        StringBuilder sb =new StringBuilder();
+        dfs(sb, 0);
+        
+        
+        
+        return answer;
+    }
+    
+    static void dfs(StringBuilder sb , int cnt){
+        int sum = 0; 
+        if(sb.toString().equals(Word)){
+            answer = count;                
+            return;
+            
+        }
+        
+        if(cnt == 5){
+            return; 
+        }
+        
+        for(int i = 0 ; i< dict.length; i++){
+            
+            sb.append(dict[i]);
+            count ++;
+            dfs(sb, cnt+1);
+            sb.deleteCharAt(sb.length()-1);
+            
+            
+        }
+            
+    } 
+}
