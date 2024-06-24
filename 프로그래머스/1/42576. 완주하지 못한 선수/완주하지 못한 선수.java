b6af438bd0f381d0816e22dbf0c29e2393c99361
ugyeong <120684605+ugyeong0u0@users.noreply.github.com>
2024-06-24 16:11:42 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 198.51 ms, Memory: 95.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        List<String> arr1 = new ArrayList<>();
+        List<String> arr2 = new ArrayList<>();
+        
+       
+        for(int i =0 ; i< participant.length; i++){
+            
+            arr1.add(participant[i]);   
+            
+        }
+        
+         for(int i =0 ; i< completion.length; i++){
+            
+            arr2.add(completion[i]);   
+            
+        }
+        
+        Collections.sort(arr1);
+        Collections.sort(arr2);
+        
+        
+        
+        for(int i =0 ; i< completion.length; i++){
+            String a = arr1.get(i);
+            String b = arr2.get(i);
+            if(!a.equals(b)){
+                return answer = a;
+            }   
+             
+        }
+
+            
+            answer = arr1.get(arr1.size()-1);
+            
+        
+       
+        
+        return answer;
+    }
+}
