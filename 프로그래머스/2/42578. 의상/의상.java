cd45767cccda67c792bb140bcc24b8e6fc4a648a
ugyeong <120684605+ugyeong0u0@users.noreply.github.com>
2024-06-24 18:14:55 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,25 @@
+import java.util. *;
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 0;
+        
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i<clothes.length ; i++){
+            
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) +1  );
+         
+            
+        }
+        int tmp =1;
+        for(String key : map.keySet() ){
+            
+            tmp *= (map.get(key)+1);
+            
+        }
+        
+        
+        return tmp-1;
+    }
+}
