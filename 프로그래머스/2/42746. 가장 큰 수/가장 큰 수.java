ca52d33a99cc7a4f368afbb391e8b1447ba4699b
ugyeong <120684605+ugyeong0u0@users.noreply.github.com>
2024-06-25 18:13:49 +0900

    [level 2] Title: 가장 큰 수, Time: 5.90 ms, Memory: 71.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        
+        String [] n = new String[numbers.length];
+        
+        for(int i = 0 ; i< numbers.length; i++){ // 사전순인경우 10<3 임!
+                
+            n[i] = numbers[i]+"";
+        }
+        
+        Arrays.sort(n, new Comparator<String>(){
+            
+            public int compare(String o1, String o2){ //하나씩만 비교 시  실행한 결괏값 "9534303" 나옴 
+                
+                return (o2+o1).compareTo(o1+o2);
+                
+            }
+            
+        });
+        for (int i =0; i<n.length;i++){
+            
+            answer += n[i];
+            
+        }
+        
+        answer= answer.charAt(0) == '0' ? "0" : answer; 
+        
+        return answer;
+    }
+}
