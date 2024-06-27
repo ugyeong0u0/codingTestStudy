3d33c80bbb9db114fca0cd76ccccee0616a8f3ea
ugyeong <120684605+ugyeong0u0@users.noreply.github.com>
2024-06-27 12:01:02 +0900

    [level 2] Title: H-Index, Time: 0.49 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        // 0 1 3 5 6
+        Arrays.sort(citations);
+        
+        int max = Integer.MIN_VALUE;
+        int len = citations.length;
+      
+        
+        int idx =Integer.MAX_VALUE; // 최소 인덱스
+        
+        for(int i = 0 ; i<= citations[len-1]; i++){
+            int k=0; 
+            for(int j =0; j<citations.length; j++){
+                
+                if(i<=citations[j]){
+                    
+                    k++;
+                    
+                }
+                
+            }
+            if(k>= i){
+                
+                max= Math.max(max, i);
+                
+            }
+       
+        }
+        if(max<-1){
+            
+            max=0;
+        }
+        
+        return max;
+    }
+}
