eeea7db859c74465aa9cc03dfcbee379d4add364
ugyeong <120684605+ugyeong0u0@users.noreply.github.com>
2024-06-28 10:19:59 +0900

    [level 1] Title: 체육복, Time: 0.56 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,65 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n-lost.length;
+        
+        List<Integer> list = new ArrayList<>();
+        
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        
+        for(int i =0 ; i< reserve.length; i++){
+            
+            list.add(reserve[i]);
+           
+        }
+        
+        
+        for(int i =0; i<lost.length;i++){
+            
+              if(list.contains(lost[i])){ // ** 다른 사람에게 체육복 못빌려줌 
+                
+              int idx = list.indexOf(lost[i]); 
+                 list.remove(idx);
+                  lost[i] =0;
+                 answer++;
+                 continue;
+            }
+            
+        }
+        
+        
+        for(int i =0; i<lost.length;i++){
+        
+        if(lost[i]!=0){
+            if(list.contains(lost[i]-1)){
+                
+                int idx = list.indexOf(lost[i]-1); 
+                list.remove(idx);
+                answer++;
+                continue;
+            }
+            
+           
+            
+           
+            if(list.contains(lost[i]+1)){
+                
+                int idx = list.indexOf(lost[i]+1); 
+                list.remove(idx);
+                answer++;
+                continue;
+            }
+            
+        }
+             
+            
+            
+            
+        }
+        
+        
+        
+        return answer;
+    }
+}
