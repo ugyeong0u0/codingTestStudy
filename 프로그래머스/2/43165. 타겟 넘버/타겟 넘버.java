5925380939158caa310e3c1502771be842cba61f
ugyeong <120684605+ugyeong0u0@users.noreply.github.com>
2024-07-02 19:09:58 +0900

    [level 2] Title: 타겟 넘버, Time: 0.40 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,32 @@
+class Solution {
+    
+    static int t ;
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        t = target;
+        
+        
+        answer = dfs(numbers, 0 ,0 ); // 주어진 배열, 현재 값의 합, 현재 인덱스, dfs 횟수 
+        
+        
+        return answer;
+    }
+    
+    public int dfs(int [] numbers, int sum, int n){
+        
+        if(n == numbers.length){
+            
+            if(sum == t){
+                
+                return 1;
+                
+            }else{
+                return 0;
+            }
+            
+        }
+       
+       return dfs(numbers, sum+numbers[n], n+1 )+dfs(numbers, sum-numbers[n], n+1 );        
+        
+    }
+}
