ddf0e0358ad57b29f058c280c99f62846b954beb
0u0 <120684605+ugyeong0u0@users.noreply.github.com>
2024-09-11 20:26:15 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -0,0 +1,32 @@
+-- 코드를 입력하세요
+SELECT 
+    TO_CHAR(SALES_DATE, 'YYYY-MM-DD') AS SALES_DATE,
+    PRODUCT_ID,
+    USER_ID,
+    SALES_AMOUNT
+    
+    FROM
+       (
+        SELECT SALES_DATE,
+                PRODUCT_ID,
+                USER_ID,
+                SALES_AMOUNT
+           FROM 
+                ONLINE_SALE
+       UNION
+           
+        SELECT SALES_DATE,
+                PRODUCT_ID,
+                NULL AS USER_ID,
+                SALES_AMOUNT
+           FROM 
+                OFFLINE_SALE
+       )
+       
+    WHERE 1=1 AND
+        TO_CHAR(SALES_DATE, 'YYYY-MM') = '2022-03'
+     
+    ORDER BY 1 ASC, 2 ASC, 3 ASC
+        
+
+    
