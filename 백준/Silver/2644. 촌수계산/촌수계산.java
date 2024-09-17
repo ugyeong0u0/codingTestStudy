6a4783fca330db4ea6d2e9f27abe82497370bd24
0u0 <120684605+ugyeong0u0@users.noreply.github.com>
2024-09-17 23:56:28 +0900

    [Silver II] Title: 촌수계산, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -1,71 +1,86 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.*;
+import java.util.*;
+/*
+ *
+ *
+ * */
 
+//class Loc{
+//    int x;
+//    int y, answer ;
+//
+//    public Loc (int x, int y){
+//        this.x= x;
+//        this.y=y;
+//    }
+//
+//
+//}
 
 public class Main {
 
-
-    static int number, target1, target2, kase, answer;
+    static int row, col, n, line, answer, tar1, tar2, k;
     static int[][] maps;
     static int[] visited;
+//    static ArrayList<Integer> list ;
+//    static int [] dx = {0,0,-1,+1};
+//    static int [] dy = {+1,-1,0,0};
 
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-
-        number = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine()); // 사람
+//        list = new ArrayList();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        target1 = Integer.parseInt(st.nextToken());
-        target2 = Integer.parseInt(st.nextToken());
-        kase = Integer.parseInt(br.readLine());
 
-        maps = new int[number + 1][number + 1];
-        visited = new int[number + 1];
-        for (int i = 0; i < kase; i++) {
+        tar1 = Integer.parseInt(st.nextToken());
+        tar2 = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(br.readLine());
+
+        answer = 0;
+        maps = new int[n + 1][n + 1];
+        visited = new int[n + 1];
+
+        for (int i = 0; i < k; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
             maps[a][b] = 1;
             maps[b][a] = 1;
-
         }
 
-        dfs(target1);
-        answer = visited[target2];
-
-        System.out.println(answer ==0 ? -1 :answer);
+        dfs(tar1, 0);
+        if (answer == 0) {
+            System.out.println(-1);
+        } else
+            System.out.println(answer);
 
     }
 
+    static void dfs(int node1, int count) {
+//        System.out.println("n1"+node1);
+        if (node1 == tar2) {
+            answer = count;
+//                System.out.println("끝"+answer);
+            return;
+        }
 
-    public static void dfs(int target) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(target);
+        for (int i = 1; i <= n; i++) {
 
-        while (!q.isEmpty()) {
-
-            int tmp = q.poll();
-
-
-            if (target2 == tmp) {
-
-                return;
-            }
-
-            for (int i = 1; i <= number; i++) {
-
-                if (visited[i] == 0 && maps[tmp][i] == 1) {
-                    q.offer(i);
-                    visited[i] = visited[tmp]+1;
-                }
-
+            if (maps[node1][i] == 1 && visited[node1] == 0) {
+//                    System.out.println(node1 + " ,,"+count);
+                visited[node1] = 1;
+                dfs(i, count + 1);
+                visited[node1] = 0;
             }
 
         }
 
+
     }
+
 }
-
-
