95ec703afd7a0103b235fa180a4c3b6e5aeaeff8
0u0 <120684605+ugyeong0u0@users.noreply.github.com>
2024-09-18 20:32:19 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -0,0 +1,19 @@
+-- 코드를 입력하세요
+SELECT
+    ANIMAL_TYPE
+    ,NAME
+    ,SEX_UPON_INTAKE
+    FROM(
+        SELECT ANIMAL_ID,ANIMAL_TYPE
+        ,SEX_UPON_INTAKE
+            ,CASE WHEN NAME IS NULL
+            THEN 'No name'
+            ELSE NAME
+            END NAME
+        FROM ANIMAL_INS
+        ORDER BY 1 ASC
+    ) 
+    
+   
+     
+    
