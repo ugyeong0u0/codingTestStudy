2e5c81cd0e1c859e7cbc32fc738864a496470964
0u0 <120684605+ugyeong0u0@users.noreply.github.com>
2024-09-22 16:09:56 +0900

    [level 1] Title: 모의고사, Time: 1.21 ms, Memory: 65.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,49 +1,56 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
-
-        int [] answer =null;
+        int[] answer;
+        ArrayList<Integer> arr = new ArrayList<>();
+        int [] su1={1,2,3,4,5};
+        int [] su2= {2,1,2,3,2,4,2,5};
+        int [] su3={3,3,1,1,2,2,4,4,5,5};
+        int [] result= new int [3]; // 결과 리턴 
         
-        int [] a = {1,2,3,4,5};
-        int [] b = {2,1,2,3,2,4,2,5};
-        int [] c = {3,3,1,1,2,2,4,4,5,5};
-        
-        int [] tmp = new int [4];
-        
-        for(int i = 0; i< answers.length ; i++){
-            if(answers[i] == a[i%a.length]){
-                tmp[1]++;
-            }
-            if(answers[i] == b[i%b.length]){
-                tmp[2]++;
-            }
-            if(answers[i] == c[i%c.length]){
-                tmp[3]++;
+        for(int i =0; i< answers.length; i++){
+            
+            int ss1 = (i-5*(i/5));
+            if(answers[i]==su1[ss1]){
+                result[0]++;
             }
             
+            int ss2 = (i-8*(i/8));
+            if(answers[i]==su2[ss2]){
+                result[1]++;
+            }
+            
+            int ss3 = (i-10*(i/10));
+            if(answers[i]==su3[ss3]){
+                result[2]++;
+               
+            }
+             // System.out.println(answers[i]+"    "+su1[ss1] +"      "+su2[ss2]);
+            
         }
+        int max =0;
+        for(int i= 0 ;i <3 ;i++){
+            
+            max= Math.max(max,result[i]);
+            
+        }
+         // System.out.println("max  "+max );
+        int c = 0;
+       
+        for(int i= 0; i<3 ;i++){
+            
+            if(max==result[i]){
+                // System.out.println("max  i "+ i );
+                c++;
+                arr.add(i+1);
+            }
+            
+        }
+        answer = new int[c];
         
-        int maxValue = Math.max(tmp[3], Math.max(tmp[1], tmp[2]));
-        List <Integer> list = new ArrayList<>();
-        if(maxValue == tmp[1]){
-            
-            list.add(1);
-            
+        for(int i=0; i<c; i++){
+            answer[i] =arr.get(i);
         }
-         if(maxValue == tmp[2]){
-            
-            list.add(2);
-            
-        }
-         if(maxValue == tmp[3]){
-            
-            list.add(3);
-            
-        }
-      
-        
-        answer = list.stream().mapToInt(Integer :: intValue).toArray();
-        
         
         return answer;
     }
