41f435c05ecaaff43a3b40530caa694952b21b87
0u0 <120684605+ugyeong0u0@users.noreply.github.com>
2024-10-15 16:20:33 +0900

    [level 2] Title: 카펫, Time: 10.64 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,41 +1,30 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-
+        int [] answer= new int[2];
+        // 카펫 가로 크기, 세로 크기 리턴 
+        // 1. 갈 + 노랑 의 약수들 곱 
+        // 2. 12 = (1,12) (2,6)(3,4)  (4,3)(6,2)(12,1)
+        // 3. 이 중에서 가 > 세 (4,3)(6,2)(12,1)
+        // 4. 세로 -2 * 세로 -2 = 노랑이가 정답 
         
-        int total = brown + yellow;
+        int total = brown + yellow; 
         
-        for(int i = 2; i<= brown; i++){ // brown까지만 for문 걸어도 됨
+        for(int i =2 ; i<= Math.abs(total); i++){
             
-            if(total%i ==0){
+            if(total %i ==0){
+                int width =Math.max(total/i,i);
+                int h =Math.min(total/i,i);
                 
-                int t1 = total/i;
-                int w = 0;
-                int h = 0;
-                if(t1 >i ){
-
-                    w= t1;
-                    h=i;
-                }else{
-                     w= i;
-                    h=t1;
+                if((width-2)*(h-2)==yellow){
+                    answer[0]= width;
+                    answer[1]= h;
+                    
                 }
-                
-                w-=2;
-                h-=2;
-                
-                if(w*h==yellow){
-
-                    answer[0]=w+2;
-                    answer[1]=h+2;
-                }
-                
                 
                 
             }
             
         }
-        
         
         
         
