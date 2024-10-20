131476171a78318bca2188ef7b534b67171921c3
0u0 <120684605+ugyeong0u0@users.noreply.github.com>
2024-10-20 09:35:37 +0900

    [level 3] Title: 네트워크, Time: 1.22 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,43 +1,51 @@
 class Solution {
-    static int answer, len ;
-    static int [][] com ;
-    static boolean [] flag;
+    
+    static int [][] map;
+    static int [][]visited;
+    static int answer;
     public int solution(int n, int[][] computers) {
         answer = 0;
-        len =n;
-        com =computers;
-        flag= new boolean [n];
-        // [[1, 1, 0],
-        //  [1, 1, 0],
-        //  [0, 0, 1]]
-        // 섬 개수 구하기 
+        map = computers;
+        visited = new int [computers.length][computers[0].length];
         
-        for(int i = 0 ; i< n;i++){
-            
-            
-            
-            if(!flag[i]){
-                answer++;
-                dfs(i); // i는 start 
+        for(int i =0; i< n; i++){
+            for(int j = 0; j<n ; j++){
+               if(visited[i][j]==0 && map[i][j]==1){
+                   
+                   answer++;
+                   visited[i][j]=1;
+                   visited[j][i]=1;
+                   
+                   dfs(i,j);
+                   
+               } 
             }
-            
         }
         
-       
         
         
         return answer;
     }
     
-    public void dfs(int start){
-        flag[start]=true;
-        for(int i = 0; i< len;i++ ){
+    static void dfs(int a, int b){
+        
+        for(int i =0;i< map.length ; i++){
             
-            if(com[start][i]==1 && flag[i] ==false){ // falg[i] 가 break 문이 될 수 있음 
-                dfs(i);
+            if(visited[b][i]==0 && map[b][i]==1){
+                
+                visited[b][i]=1;
+                visited[i][b]=1;
+                dfs(b,i);
+        
+                
             }
+        
         }
-            
+        
+        
         
     }
+    
+    
+    
 }
