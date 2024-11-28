34b04d6c840d012e4f6fb50ef041eeac193117aa
0u0 <120684605+ugyeong0u0@users.noreply.github.com>
2024-11-28 23:38:41 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,20 @@
+-- 코드를 입력하세요
+SELECT '/home/grep/src/' || BOARD_ID  || '/' ||  FILE_ID || FILE_NAME || FILE_EXT AS FILE_PATH
+    FROM (
+        SELECT B.FILE_ID,
+            B.FILE_EXT,
+            B.FILE_NAME,
+            B.BOARD_ID
+        FROM USED_GOODS_BOARD A ,
+            USED_GOODS_FILE B
+        WHERE 1=1
+            AND A.BOARD_ID= B.BOARD_ID
+            AND A.VIEWS = (
+                SELECT MAX(VIEWS)
+                FROM USED_GOODS_BOARD
+            ) 
+        
+        ORDER BY 1 DESC
+    )
+    
+    
