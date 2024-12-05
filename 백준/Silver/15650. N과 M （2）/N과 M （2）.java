36bd8e1c013b9f479d30f92db68073d25ee5dd98
0u0 <120684605+ugyeong0u0@users.noreply.github.com>
2024-12-06 03:28:35 +0900

    [Silver III] Title: N과 M (2), Time: 116 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,40 +1,88 @@
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.*;
+import java.sql.SQLOutput;
+import java.util.*;
+/*
+ *
+ *
+ * */
+
+//class Loc {
+//    int x;
+//    int y, answer;
+//
+//
+//    public Loc(int x, int y) {
+//        this.x = x;
+//        this.y = y;
+//
+//    }
+//
+//
+//}
+
 public class Main {
- 
-	public static int[] arr;
-	public static int N, M;
-	public static StringBuilder sb = new StringBuilder();
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
- 
-		N = in.nextInt();
-		M = in.nextInt();
- 
-		arr = new int[M];
-        
-		dfs(1, 0);
-		System.out.print(sb);
- 
-	}
- 
-	public static void dfs(int at, int depth) {
- 
-		if (depth == M) {
-			for (int val : arr) {
-				sb.append(val).append(' ');
-			}
-			sb.append('\n');
-			return;
-		}
-        
-		for (int i = at; i <= N; i++) {
- 
-			arr[depth] = i;
-			dfs(i + 1, depth + 1);
- 
-		}
-	}
+
+    static int n, answer, sum, arrN;
+    static int[] maps;
+    static ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st =new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken()); // 테케 수
+        arrN = Integer.parseInt(st.nextToken());
+
+
+
+        answer = 0;
+        sum = 0;
+        maps = new int[n+1]; // 인덱스가 행, 값이 열
+
+        /*
+         * 1. 몇 번 반복할지 -> 재귀호출로 정함
+         * 2.
+         *
+         * */
+        ArrayList<Integer> list2 = new ArrayList<>();
+        bfs(0, 1,list2);
+
+//        Collections.sort(list);
+
+//        System.out.println(list.size());
+
+        for(int i= 0; i<list.size(); i++){
+            ArrayList<Integer> tmp =list.get(i);
+            for(int j =0 ; j< tmp.size(); j++){
+                System.out.print(tmp.get(j)+" ");
+            }
+            System.out.println();
+
+        }
+
+
+
+    }
+
+    static void bfs(int dept, int next, ArrayList<Integer> arr3) { // dept
+
+        if(dept == arrN){
+            list.add(new ArrayList<>(arr3)); // 얼레이리스트는 참조로 저장되기 때문에 최종 arr3가 list에 저장됨. 복사본 저장이 필요
+            return;
+        }
+
+
+        for(int i =next; i<= n; i++){
+
+            arr3.add(i);
+//            System.out.println("결과"+arr3.get(arr3.size()-1));
+            bfs(dept+1, i+1, arr3);
+            arr3.remove(arr3.size()-1);
+
+        }
+    }
+
+
 }
