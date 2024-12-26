2e04cf253ec98f353eabbcf388654d9b9f3f6c7d
0u0 <120684605+ugyeong0u0@users.noreply.github.com>
2024-12-26 18:01:12 +0900

    [Silver IV] Title: 동전 0, Time: 108 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,32 +1,91 @@
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.*;
+import java.sql.SQLOutput;
+import java.util.*;
+
+class loc{
+    int x;
+    int y;
+
+    loc(int x, int y){
+
+        this.x=x;
+        this.y=y;
+    }
+}
+
+
 public class Main {
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
-		
-		int N = in.nextInt();
-		int K = in.nextInt();
-		
-		int[] coin = new int[N];
-		
-		for(int i = 0; i < N; i++) {
-			coin[i] = in.nextInt();
-		}
-		
-		int count = 0;
- 
-		for(int i = N - 1; i >= 0; i--) {
- 
-			// 현재 동전의 가치가 K보다 작거나 같아야지 구성가능하다.
-			if(coin[i] <= K) {
-				// 현재 가치의 동전으로 구성할 수 있는 개수를 더해준다.
-				count += (K / coin[i]);
-				K = K % coin[i];
-			}
-		}
-		System.out.println(count);
-	}
- 
+
+    static int n, answer, sum, m;
+//    static char[][] maps;
+    static int []visited;
+//    static int [] visited2;
+//    static int [] ddx ={0,0,1,-1};
+//    static int [] ddy ={1,-1,0,0};
+//    static ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st =new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken()); // 테케 수 10
+        m = Integer.parseInt(st.nextToken()); // 총 가격 4200
+
+        answer = Integer.MAX_VALUE;
+//        sum = 0;
+
+        visited = new int[n]; // 지폐 값
+
+
+        for(int i= 0; i<n; i++){
+            visited[i] = Integer.parseInt(br.readLine());
+        }
+//        Arrays.sort(visited);
+
+
+        /*
+        *1. for문을 통해 현재 지폐가 값에 쓰일 수 있는지
+        * 있다면 dfs 또 보내기(단 현재 빼고 남은게 현재 지폐보다 크면 안됨)
+        *2. dfs(현재 지폐 인덱스 번호, 남은 금액, 사용한 지폐 )
+        *3. break 문은 현재 지폐가격일때
+        * */
+
+        dfs(n-1, m,0);
+
+//        if(answer )
+
+        System.out.println(answer);
+
+
+
+
+
+    }
+
+
+    static void dfs(int idx, int change, int dept){
+
+        if(change==0){
+            answer= Math.min(answer, dept);
+            return;
+        }
+//        System.out.println("visited[idx] "+ visited[idx]+ " change :  "+ change);
+
+        for(int i = idx; i>=0 ;i--) {
+            int k = change / visited[i];
+            if (k > 0) {
+
+//                System.out.println("visited[idx]   "+ visited[i]+ "    change :  "+ change +  "   k  " +k);
+                dfs(i-1, change - visited[i]*k, dept +k);
+
+            }else{
+                if(i-1>=0)
+                    dfs(i-1, change, dept);
+            }
+        }
+    }
+
+
 }
