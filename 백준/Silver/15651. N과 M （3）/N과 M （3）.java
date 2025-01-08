08ba8d7421ee241583086ef67b7f3401b6cebad6
0u0 <120684605+ugyeong0u0@users.noreply.github.com>
2025-01-09 02:43:01 +0900

    [Silver III] Title: N과 M (3), Time: 388 ms, Memory: 67828 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,39 +1,73 @@
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.*;
+import java.sql.SQLOutput;
+import java.util.*;
+
+//class Loc {
+//    int m; // 보석 무게
+//    int v; // 보석 가격
+//
+//    Loc(int m, int v) {
+//
+//        this.m = m;
+//        this.v = v;
+//    }
+//}
+
+
 public class Main {
-	
-	public static int[] arr;
-	public static int N, M;
-	public static StringBuilder sb = new StringBuilder();
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
- 
-		N = in.nextInt();
-		M = in.nextInt();
- 
-		arr = new int[M];
-        
-		dfs(0);
-		System.out.print(sb);
- 
-	}
- 
-	public static void dfs(int depth) {
-		// 깊이가 M이랑 같아지면 출력후 return
-		if (depth == M) {
-			for (int i = 0; i < M; i++) {
-				sb.append(arr[i] + " ");
-			}
-			sb.append('\n');
-			return;
-		}
- 
-		for (int i = 1; i <= N; i++) {
-			arr[depth] = i;
-			dfs(depth + 1);
-		}
-	}
- 
+
+    static int n, m;
+    static StringBuilder answer;
+    static int [] arr;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken()); // 몇번째까지 구할지
+        m = Integer.parseInt(st.nextToken());
+
+        answer = new StringBuilder();
+        arr= new int [m];
+        /*
+        *
+        *
+        * */
+        boolean [] visited = new boolean[n+1];
+        dfs(0);
+
+
+        System.out.println(answer);
+
+
+    }
+
+    static void dfs(int dept){
+
+        if(dept==m){
+            for(int i = 0; i<dept; i++){
+                answer.append(arr[i]).append(" ");
+            }
+            answer.append("\n");
+            return;
+        }
+
+        for(int i=1; i<=n ; i++){
+//            if(!v[i]){
+//                v[i]=true;
+            arr[dept] = i;
+            dfs(dept+1);
+//                v[i] =false;
+//            }
+        }
+
+
+
+    }
 }
+
+
+
