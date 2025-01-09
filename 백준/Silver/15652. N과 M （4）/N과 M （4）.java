6e727067c4410cb91d2af4f35673086634811697
0u0 <120684605+ugyeong0u0@users.noreply.github.com>
2025-01-09 18:09:25 +0900

    [Silver III] Title: N과 M (4), Time: 128 ms, Memory: 16972 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.*;
+import java.sql.SQLOutput;
+import java.util.*;
+
+//class Loc {
+//    int m; // 보석 무게
+//    int v; // 보석 가격
+//
+//    Loc(int m, int v) {
+//
+//        this.m = m;
+//        this.v = v;
+//    }
+//}
+
+
+public class Main {
+
+    static int n, m;
+    static StringBuilder answer;
+    static int [] arr;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken()); // 몇번째까지 구할지
+        m = Integer.parseInt(st.nextToken());
+
+        answer = new StringBuilder();
+        arr= new int [m];
+        /*
+        *
+        *
+        * */
+        boolean [] visited = new boolean[n+1];
+        dfs(0,1);
+
+
+        System.out.println(answer);
+
+
+    }
+
+    static void dfs(int dept, int next){
+
+        if(dept==m){
+            for(int i = 0; i<dept; i++){
+                answer.append(arr[i]).append(" "); // arr은 전역으로 해도되는이유는 dept ==m 인 경우엔 덮어씌어지기 때문
+            }
+            answer.append("\n");
+            return;
+        }
+
+        for(int i=next; i<=n ; i++){
+//            if(!v[i]){
+//                v[i]=true;
+            arr[dept] = i; //****
+            dfs(dept+1, i);
+//                v[i] =false;
+//            }
+        }
+
+
+
+    }
+}
+
+
+
