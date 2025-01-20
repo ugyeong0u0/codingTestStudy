384c8cad703d1d05bd5820b5cef7e99625b0dd99
0u0 <120684605+ugyeong0u0@users.noreply.github.com>
2025-01-20 22:25:11 +0900

    [Silver III] Title: 계단 오르기, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,46 +1,120 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-import java.util.*;
-import java.io.*;
-// 바텀업 방식
-
-/*n을 1과 2로 분해 시키기
-단, 1이 연속으로 3번나오면안됨 즉 2번까지 가능,
-딘, 1이 띄어져서 나오는건 가능 */
-
-/*해결방법 : top down 방식(재귀),bottomup방식(for, 배열에 넣는 메모라이제이션 방법)
- *  현재 i칸 /두칸전(i-2) 값, (i-1)값+(i-3) 중
-큰 값을 현재 i번째 계단의 값과 합하여 함.
--> 주의 : i가 3미만의 수라면 음수로 인덱스 아웃오브 바운드 뜸 */
 public class Main {
-	static int n;
-	static int stair[];
-	static Integer dp[];
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
 
-		stair = new int[n + 1];
-		dp = new Integer[n + 1]; // Integer 객체 배열은 default값이 null
+    static int n, answer;
+    static int[] stairs;
+    static int[] nums, visited;
 
-		for (int i = 1; i < n + 1; i++)
-			stair[i] = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws IOException {
+        /**
+         *
+         * 1. 전체 숫자를 1과 2를 통해 조합,
+         *  112 는 됨 ***
+         *  121는 됨 ****
+         *
+         *  211 안됨  => 차라리 계단 visited를 추가해서 visited[] 전에 하나 체크 될때마다 +1
+         *
+         *  1. 1이 세번나오면 안됨
+         *  2. 2 1 1
+         * 2. 6을 만드는
+         *  for문
+         *      1) 2로 나눠서 몫
+         *      2) 남은거 1
+         *
+         *  dfs(int 1의 개수 세기  )를 돌림
+         *  break문은 for문으로 센거
+         *
+         *
+         *
+         *
+         *
+         */
 
-		dp[1] = stair[1];
-		dp[0] = stair[0]; // 인티저 객체 배열은 디폴트값이 null이라 초기화필요
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        stairs = new int[n + 1];
+//        nums = new int[3]; // 1과 2가 몇개 있는지
 
-		if (n >= 2) { // **
-			dp[2] = stair[1] + stair[2];
-		}
+        for (int i = 1; i <= n; i++) {
+            stairs[i] = Integer.parseInt(br.readLine());
+        }
+        visited= new int [n+1]; // 계단 지나간 값 표시
 
-		System.out.println(find(n));
-	}// main
+        visited[1]=stairs[1];
+        if(n>=2)
+            visited[2] = Math.max(visited[1]+stairs[2], stairs[2]); // 12, 2
+        if(n>=3)
+            visited[3] = Math.max(visited[1]+stairs[3], stairs[2]+stairs[3]); // 13, 23
+        // 4= visited[2] + st[4](24), visi[1] + st3  + statir[4](134,  124)
+        // 5 = visiited[3] + st[5]( 235,135), visited[2] + st[4] st[5]  (245, 1245)
 
-	static int find(int n) { // 반환값
-		if (dp[n] == null) // **
-			dp[n] = Math.max(find(n - 2), find(n - 3) + stair[n - 1]) + stair[n];
-		return dp[n];
-	}
+        for(int i =4 ; i<= n ;i++){
 
+            visited[i] = Math.max(visited[i-2], visited[i-3]+stairs[i-1])+stairs[i];
+
+        }
+
+
+
+
+//        int tmp = n / 2;
+//        answer = Integer.MIN_VALUE;
+//        for (int i = tmp; i >= 0; i--) { // 6/2 =3,
+//
+//
+//            int tmp2 = n - (i * 2); // 6 - 2*3 =0
+//
+//            nums[1] = tmp2;
+//            nums[2] = i;
+//
+//            int dept = i + tmp2; // 총 반복 횟수 3번
+//
+//
+//            dfs(0, 0, 0, 0, dept); // 1의 개수 , 반복 횟수
+//
+//
+//        }
+
+
+        System.out.println(visited[n]);
+    }
+
+    static void dfs(int cnt, int sum, int now, int lv, int dept) { // 연속된거 개수 ,지난계단 지났는지, 점수합, 현재 몇칸에 있는지, 반복몇번했는지, 주어진 반복횟수
+
+        // 11 2 11
+        if (cnt == 2) { //  *****
+//            System.out.print("-----xxx");
+            return;
+        }
+
+        if (lv == dept) {
+            answer = Math.max(answer, sum);
+//            System.out.println(a + "    " +sum);
+            return;
+
+        }
+
+
+        for (int i = 1; i <= 2; i++) { // st[2] = 3;
+            if (nums[i] > 0) {
+                nums[i]--;
+                int prev = now;
+                now += i;
+
+                if (prev!= 0 && prev+1 == now ) { // prev =4, now 5
+//                    System.out.print(cnt+ " +1");
+
+                    dfs(cnt+1, sum + stairs[now], now, lv + 1, dept);
+                } else { // 2인 경우
+                    dfs(0, sum + stairs[now], now, lv + 1, dept);
+                }
+                now -= i;
+                nums[i]++;
+            }
+        }
+    }
 }
