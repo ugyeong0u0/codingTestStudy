a47ad43b0db962f05e14f90e30f06aeb7221e716
0u0 <120684605+ugyeong0u0@users.noreply.github.com>
2025-01-25 21:02:51 +0900

    [Silver III] Title: 2×n 타일링, Time: 104 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,17 +1,37 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    static int n, answer;
+    static int [] dp;
+    public static void main(String[] args) throws IOException  {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        dp= new int [n+1];
 
-        int[] dp = new int[1001];
+
+
         dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i <= n; i++)
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        if (n>=2)
+            dp[2] = 2;
 
-        System.out.println(dp[n]);
+
+        /**
+         * dp[4] = 1+3, 2+2 = dp[3] + dp[2] = 5
+         * dp[5] = 
+         *
+         */
+
+        System.out.println(dfs(n));
+    }
+
+    static int dfs(int n ){
+
+        if(dp[n]==0 && n>=3){
+            dp[n]= (dfs(n-1)+dfs(n-2))%10007;
+        }
+        return dp[n];
 
     }
 }
