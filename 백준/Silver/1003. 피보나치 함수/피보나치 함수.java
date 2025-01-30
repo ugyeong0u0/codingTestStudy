d29f2daaee5e61d0cbc9b30833d5ef2d5e5f72d7
0u0 <120684605+ugyeong0u0@users.noreply.github.com>
2025-01-30 17:16:50 +0900

    [Silver III] Title: 피보나치 함수, Time: 104 ms, Memory: 14052 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n;
+    static Integer [][] dp ;
+
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st= new StringTokenizer(br.readLine());
+
+
+        n = Integer.parseInt(br.readLine());
+
+        dp= new Integer [41][2];
+
+        dp[0][0]=1;
+        dp[0][1]=0;
+        dp[1][0]=0;
+        dp[1][1]=1;
+
+        StringBuilder sb =new StringBuilder();
+        for(int i = 0; i< n; i++){
+
+            int k =  Integer.parseInt(br.readLine());
+
+            sb.append(dfs(k,0)).append(" ").append(dfs(k,1)).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+
+    public static int dfs(int k, int l){
+
+        if(k>1 && dp[k][l]== null){
+
+            dp[k][l]= dfs(k-1, l)+ dfs(k-2,l);
+            // 3 = 2,0 + 1,0
+            // 2 = 1,0 + 0,0
+        }
+//        System.out.print(k+ "의"+l+"은"+dp[k][l]);
+//        System.out.println("k :   "+ k+ " l  "+ l  +  "   리턴 되는값    " + dp[k][l]);
+        return dp[k][l];
+
+    }
+}
