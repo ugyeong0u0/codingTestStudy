26cc232e2296117a6fb07982b9ca09b394cdbcff
0u0 <120684605+ugyeong0u0@users.noreply.github.com>
2025-02-01 13:20:17 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 128 ms, Memory: 14540 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -1,38 +1,33 @@
-import java.util.*;
-import java.math.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-	
-	static int dp[],cost[] ;
-	
-	public static void main(String[] args)   {
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		//dp 배열과 비용배열 선언
-		dp = new int[n+1];
-		cost = new int[n+1];
-        
-		for(int i=1;i<=n;i++) {
-			cost[i] = sc.nextInt();
-		}
-		
-		dp[1] = cost[1]; // dp[1]은 항상 cost[1]로 초기화 된다. 이전에 더해진 값이 없기 때문
-		
-		for(int i=2;i<=n;i++) { //두 번째 숫자를 기준으로 n까지 반복
-			dp[i] = cost[i]; // 우선 자신의 값을 dp에 저장해 둔다.
-			for(int j=1;j<i;j++) { // 첫 번째 부터 i이전 까지 비교를 위한 반복
-				if(cost[i]>cost[j]) { // 기준값이 더 큰 경우
-					dp[i] = Math.max(dp[j]+cost[i],dp[i]); // 증가 수열이므로 dp값 갱신
-				}
-			}
-		}
-		int max = Integer.MIN_VALUE;
-		for(int i=1;i<=n;i++) {
-			if(dp[i]>max) {
-				max = dp[i];
-			}
-		}
-		System.out.println(max);
-	}
-	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] A = new int[n];
+        int[] dp = new int[n];  // dp[i] : i번째 원소를 마지막으로 하는 가장 큰 증가하는 부분 수열의 합
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+            dp[i] = A[i]; // 초기값은 자기 자신
+        }
+
+        int answer = dp[0];
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (A[i] > A[j]) { // 증가하는 부분 수열 조건
+                    dp[i] = Math.max(dp[i], dp[j] + A[i]);
+                }
+            }
+            answer = Math.max(answer, dp[i]); // 최댓값 갱신
+        }
+
+        System.out.println(answer);
+    }
 }
