a26da5d79cf24a6c875eab6e4eb22d2c2fb3a825
0u0 <120684605+ugyeong0u0@users.noreply.github.com>
2025-02-01 15:49:58 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 124 ms, Memory: 14544 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -1,33 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static int n, answer;
+    static int[] dp, visited;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        int[] A = new int[n];
-        int[] dp = new int[n];  // dp[i] : i번째 원소를 마지막으로 하는 가장 큰 증가하는 부분 수열의 합
+        n = Integer.parseInt(br.readLine());
 
+
+        dp = new int[n];
+        visited = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
-            dp[i] = A[i]; // 초기값은 자기 자신
+
+            int k = Integer.parseInt(st.nextToken());
+
+            dp[i] = k;
         }
 
-        int answer = dp[0];
+        visited[0] = dp[0];
+
+
 
         for (int i = 1; i < n; i++) {
+            visited[i]=dp[i];
             for (int j = 0; j < i; j++) {
-                if (A[i] > A[j]) { // 증가하는 부분 수열 조건
-                    dp[i] = Math.max(dp[i], dp[j] + A[i]);
+                if (dp[i]>dp[j]) {
+                    visited[i] = Math.max(visited[j]+dp[i], visited[i]);
                 }
             }
-            answer = Math.max(answer, dp[i]); // 최댓값 갱신
+
+        }
+
+        for(int a : visited){
+            answer = Math.max(answer, a);
         }
 
         System.out.println(answer);
+
     }
 }
