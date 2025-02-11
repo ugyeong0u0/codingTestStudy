b035d3d779cdc6b1c635f3ec862048f052ff9c6d
0u0 <120684605+ugyeong0u0@users.noreply.github.com>
2025-02-11 14:46:57 +0900

    [Silver II] Title: 로또, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,82 +1,57 @@
-    import java.io.BufferedReader;
-    import java.io.IOException;
-    import java.io.InputStreamReader;
-    import java.io.*;
-    import java.sql.SQLOutput;
-    import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-    //class Loc {
-    //    int m; // 보석 무게
-    //    int v; // 보석 가격
-    //
-    //    Loc(int m, int v) {
-    //
-    //        this.m = m;
-    //        this.v = v;
-    //    }
-    //}
+public class Main {
+    static int[] a = new int[6];
+    static StringBuilder sb = new StringBuilder();
+    static int[] arr, visited;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public class Main {
+        int n = 1;
 
-        static int n;
-        static ArrayList<int []> arr1 = new ArrayList<>(); // 실제 배열 들어가 있는 곳
+        while (n != 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken()); // 테스트 케이스 개수, 맨 앞 글자
+            arr = new int[n];
+            visited = new int[n];
 
-        static StringBuilder sb = new StringBuilder();
-
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-            n = 1;
-            while(n!=0){
-                StringTokenizer st = new StringTokenizer(br.readLine());
-
-                n = Integer.parseInt(st.nextToken());
-
-                int [] tmp = new int[n];
-                int i = 0;
-                while(st.hasMoreTokens()){
-                    tmp[i++] = Integer.parseInt(st.nextToken());
-                }
-                arr1.add(tmp);
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
             }
 
-            for(int i = 0 ; i< arr1.size(); i++){
-                int [] tmpArr = arr1.get(i);
-                int [] co = new int [6];
-
-                dfs(tmpArr, 0, co, 0);
-                sb.append("\n");
-
-            }
-
-
-            System.out.println(sb);
+            dfs(0,0);
+            sb.append("\n");
 
 
         }
 
-        static void dfs(int [] target , int dept, int [] co, int next){
 
-            if(dept == 6){
-                for(int i =0 ;i< co.length ; i++){
-                    sb.append(co[i] + " ");
-                }
-                sb.append("\n");
-                return;
-            }
-
-            for(int i=next; i<target.length;i++){
-
-                co[dept]=target[i];
-                dfs(target, dept+1, co, i+1);
-
-            }
-
-        }
-
+        System.out.println(sb);
 
     }
 
+    static void dfs(int dept, int next) {
+        if (dept == 6) {
+            for (int i : a) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
 
+        for (int i = next; i < arr.length; i++) {
+            if (visited[i]==0){
+                visited[i]=1;
+                a[dept] = arr[i];
+                dfs(dept+1, i+1);
+                visited[i]=0;
+            }
 
+        }
+
+    }
+}
