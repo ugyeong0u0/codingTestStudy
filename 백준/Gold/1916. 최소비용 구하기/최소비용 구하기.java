c6bd221689015bc56ca49aa15ad1077594dc095a
0u0 <120684605+ugyeong0u0@users.noreply.github.com>
2025-02-25 21:20:16 +0900

    [Gold V] Title: 최소비용 구하기, Time: 464 ms, Memory: 51672 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -4,28 +4,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-class Circle{
-    int st;
-    int end ;
+class Circle implements Comparable<Circle> {
+//    int st;
+    int end;
     int cost;
 
-    public Circle(int st, int end,int cost){
-        this.st = st;
-        this.end = end ;
-        this.cost =cost;
+    public Circle( int end, int cost) {
+//        this.st = st;
+        this.end = end;
+        this.cost = cost;
     }
 
+    public int compareTo(Circle tmp) {
+        return this.cost - tmp.cost;
+    }
+    public String toString() {
+        return   "->"+ end + " 비용: "+ cost + "   \\\\" ;
+    }
 }
 
-public class Main {
+public class Main{
 
     static StringBuilder sb = new StringBuilder();
-    static int [] dist; // 거리
+    static int[] dist; // 거리
+    static int[] visited; // 방문 여부
 
     static int n, m, a, b;
 
     static ArrayList<Circle> graph;
-
+    //    static Queue<Integer> q = new LinkedList<>();
+    static ArrayList<ArrayList<Circle>> maps;
+    static PriorityQueue<Circle> pq = new PriorityQueue<>();
 
 
     public static void main(String[] args) throws IOException {
@@ -38,42 +47,51 @@ public class Main {
 
         // 배열 초기화
         graph = new ArrayList<>();
-        dist = new int [n+1];
-        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist = new int[n + 1];
+        maps = new ArrayList<>();
+        visited = new int[n + 1];
 
-        StringTokenizer st;
-        for(int i =0; i<m ; i++){
-            st = new StringTokenizer(br.readLine());
-            int o1= Integer.parseInt(st.nextToken());
-            int o2= Integer.parseInt(st.nextToken());
-            int o3= Integer.parseInt(st.nextToken());
+        Arrays.fill(dist, Integer.MAX_VALUE);
 
-            graph.add(new Circle(o1,o2,o3));
+        for (int i = 0; i <= n; i++) {
+            maps.add(new ArrayList<>()); // **
         }
 
 
+        StringTokenizer st;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int o1 = Integer.parseInt(st.nextToken());
+            int o2 = Integer.parseInt(st.nextToken());
+            int o3 = Integer.parseInt(st.nextToken());
+
+            maps.get(o1).add(new Circle(o2, o3));
+
+
+        }
+
         st = new StringTokenizer(br.readLine());
-        a= Integer.parseInt(st.nextToken());
-        b= Integer.parseInt(st.nextToken());
+        a = Integer.parseInt(st.nextToken()); // 출발지
+        b = Integer.parseInt(st.nextToken()); // 도착지
 
+        pq.offer(new Circle(a, 0));
+        dist[a] = 0;
 
-        dist[a] =0;
-
-        for(int i = 0; i<n; i++){
-            for(int j =0 ; j< m ;j++){
-
-                Circle tmp = graph.get(j);
-
-                if(dist[tmp.st]!=Integer.MAX_VALUE && dist[tmp.end] > dist[tmp.st]+tmp.cost){
-                    dist[tmp.end] = dist[tmp.st]+tmp.cost;
+        while (!pq.isEmpty()) {
+            Circle now = pq.poll();
+            if (visited[now.end] ==0) {
+                visited[now.end] = 1;
+                for (Circle tmp :maps.get(now.end)) {
+                    if(visited[tmp.end]==0 && dist[tmp.end]> dist[now.end]+ tmp.cost)  {
+                        dist[tmp.end] = dist[now.end] + tmp.cost;
+                        pq.offer(new Circle(tmp.end, dist[tmp.end]));
+                    }
                 }
-
             }
         }
 
 
         System.out.println(dist[b]);
-
     }
 
 
