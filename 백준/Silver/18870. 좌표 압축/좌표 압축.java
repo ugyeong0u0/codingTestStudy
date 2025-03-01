f19e4186687978036a479f13491ddaa13bc9dcde
0u0 <120684605+ugyeong0u0@users.noreply.github.com>
2025-03-02 00:12:18 +0900

    [Silver II] Title: 좌표 압축, Time: 1984 ms, Memory: 252404 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,53 +1,139 @@
-import java.util.Scanner;
-import java.util.HashMap;
-import java.util.Arrays;
- 
- 
+import javax.crypto.spec.PSource;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+//class Circle implements Comparable<Circle> {
+//    int st;
+//    int w;
+//
+//    public Circle(int st, int w) {
+//        this.st = st;
+//        this.w = w;
+//    }
+//
+//    public int compareTo(Circle tmp) {
+//        return w - tmp.w;
+//    }
+//
+//
+////    public String toString() {
+////        return   "->"+ end + " 비용: "+ cost + "   \\\\" ;
+////    }
+//}
+
 public class Main {
- 
-	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
-		
-		int N = in.nextInt();
-		
-		int[] origin = new int[N];	// 원본 배열
-		int[] sorted = new int[N];	// 정렬 할 배열
-		HashMap<Integer, Integer> rankingMap = new HashMap<Integer, Integer>();	// rank를 매길 HashMap
- 
-		
-		for(int i = 0; i < N; i++) {
-			// 정렬할 배열과 원본 배열에 값을 넣어준다.
-			sorted[i] = origin[i] = in.nextInt();
-		}
-		
-		// 정렬 할 배열에 대해 정렬을 수행해준다.
-		Arrays.sort(sorted);
-		
-		
-		// 정렬 된 배열을 순회하면서 map에 넣어준다.
-		int rank = 0;
-		for(int v : sorted) {
-			/*
-			 *  이 때 만약 앞선 원소가 이미 순위가 매겨졌다면
-			 *  중복되면 안되므로, 원소가 중복되지 않을 때만
-			 *  map에 원소와 그에 대응되는 순위를 넣어준다.
-			 */
-			if(!rankingMap.containsKey(v)) {
-				rankingMap.put(v, rank);
-				rank++;		// map에 넣고나면 다음 순위를 가리킬 수 있도록 1을 더해준다.
-			}
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		for(int key : origin) {
-			int ranking = rankingMap.get(key);	// 원본 배열 원소(key)에 대한 value(순위)를 갖고온다.
-			sb.append(ranking).append(' ');
-		}
-		
-		System.out.println(sb);
-		
-		
- 
-	}
+
+    static StringBuilder sb = new StringBuilder();
+    static int[] arr, answer; // 거리
+
+    static HashMap<Integer,Integer> map = new HashMap<>();
+    static int t, n, m, count, startNode;
+    static int startPoint = 0;
+//    static ArrayList<Circle>[] maps;
+//    static int[] check;
+
+//    static final int INF = 10_000_000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+        // 변수 초기화
+        n = Integer.parseInt(br.readLine()); // 좌표 개수
+//        m = Integer.parseInt(st.nextToken()); // 비행기 개수
+//        startNode = Integer.parseInt(br.readLine());
+//        check = new int[n + 1];
+
+//        dist = new int[n + 1];
+//        maps = new ArrayList[n + 1];
+
+        arr = new int[n];
+        answer= new int [n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+//            maps[i] = new ArrayList<>(); // **
+            arr[i] = Integer.parseInt(st.nextToken());
+            answer[i]= arr[i];
+        }
+
+        Arrays.sort(arr);
+
+
+
+
+
+        int k = 0;
+
+        // 상한 하한 정해야 같은 수로 만듦
+
+        int i = 0;
+        while (i < n) {
+            int start = findFirst(arr[i]);
+            int end = findEnd(arr[i]);
+//            System.out.println(k);
+
+//            System.out.println(arr[i] + "  " + start+"   "+ end);
+
+//            System.out.println(i);
+            map.put(arr[i], k);
+
+            for (int j = start; j < end; j++) {
+               i++;
+            }
+
+
+            k++;
+        }
+
+        for(int j =0 ;j <n;j++){
+            sb.append(map.get(answer[j])).append(" ");
+        }
+
+
+        System.out.println(sb);
+    }
+
+    static int findFirst(int target) {
+        int l = startPoint;
+        int r = arr.length - 1;
+
+        // 하한 구하기
+        while (l < r) {
+            int mid = (l + r) / 2;
+
+            if (arr[mid] >= target) { //중요한건 mid 값이 target 일때 어디를 높일건지임 이건 내가 하한을 idx로해서 이렇게되니 그걸 기준으로 생각하기
+                r = mid; // mid-1 이든 상관 x ? 있음 타겟 인덱스가 r이 되어버림 근데 m는 /2할때 내림이라 전 mid이자 현재 r에 mid가 될 수 없음!!
+            } else if (arr[mid] < target) {
+                l = mid + 1; // ㅣ은 mid 구할때 /2는 내림해버려서 무조건 +1 해줘야함 안그럼 mid/2 == l하고 같아져서 무한루프돔
+            }
+
+        }
+        return l;
+
+
+    }
+
+    // 상한 구하기
+    static int findEnd(int target) {
+        int l = startPoint;
+        int r = arr.length; // ** 상한은 idx+1이기 때문
+
+
+        while (l < r) {
+            int mid = (l + r) / 2;
+
+            if (arr[mid] > target) {
+                r = mid;
+            } else if (arr[mid] <= target) {
+                l = mid + 1;
+            }
+
+        }
+        return l;
+
+    }
+
+
 }
+
