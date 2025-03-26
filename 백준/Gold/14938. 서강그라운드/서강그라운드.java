d7a9eafed479c1a364f7ff52f6d03d409061b2b3
0u0 <120684605+ugyeong0u0@users.noreply.github.com>
2025-03-26 21:08:46 +0900

    [Gold IV] Title: 서강그라운드, Time: 108 ms, Memory: 14428 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.java
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.java
@@ -1,7 +1,7 @@
     import java.util.*;
     import java.io.*;
 
-    class G{
+    class G implements Comparable<G>{
         int st;
         int end;
         int cost;
@@ -12,13 +12,18 @@
             this.cost = cost;
         }
 
+        public int compareTo(G o1){
+            return this.cost- o1.cost;
+        }
+
 
     }
     public class Main {
         static int n, m, r;
         static int [] map;
-        static int [] [] arr;
+        static int [] answer;
 
+        static ArrayList<G> [] maps;
         static int INF = 16;
         public static void main(String[] args) throws IOException {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,24 +31,26 @@
             n = Integer.parseInt(st.nextToken()); // 지역의 개수
             m = Integer.parseInt(st.nextToken()); // 수색범위
             r= Integer.parseInt(st.nextToken()); // 길의 개수
+            maps = new  ArrayList [n+1];
+            answer = new int [n+1];
+
+            for(int i =1 ; i<=n ; i++){
+
+                maps[i] = new ArrayList<G>();
+
+            }
+
+
             map = new int [n+1];
-            arr= new int [n+1][n+1];
-            int [] answer = new int [n+1];
-            int [][] visited = new int [n+1][n+1];
+
+
 
 
             st = new StringTokenizer(br.readLine());
             for(int i =1; i<= n; i++){
                 map[i]= Integer.parseInt(st.nextToken());
             }
-            for(int i =1 ; i<=n ; i++){
-                for(int j = 1; j <=n ; j++){
-                    if(i==j) continue;
-                    arr[i][j] = INF;
 
-                }
-
-            }
 
 
             for(int i =0 ; i< r ; i++){
@@ -53,45 +60,70 @@
                 int b = Integer.parseInt(st.nextToken());
                 int c = Integer.parseInt(st.nextToken());
 
-                arr[a][b]= c;
-                arr[b][a]= c;
+                maps[a].add(new G(a,b,c));
+                maps[b].add(new G(b,a,c));
 
             }
 
+            int [] answer3 = new int [n+1];
+
+            for(int i =1 ; i<=n ; i++){
 
 
-            for(int i =1; i <=n; i++){
-                for(int j = 1; j<=n ; j++){
-                    for(int k= 1; k<=n ; k++){
-                        if(arr[j][i]+arr[i][k] < arr[j][k] ){
-//                            System.out.println("m "+ m);
-//                            if(visited[j][k]==0){ // 있으면 1->2 ->3 과 1->4 ->3 이거 비교 불가함
 
-//                                visited[j][k]=1;
 
-//                                System.out.print( j+ " -> "+ i  + "  " +i + " -> "+k + " : " +arr[j][i] + "   +  "+arr[i][k] + "    "+ "   vs "  + arr[j][k]  );
-//                                System.out.println();
-                                arr[j][k] = arr[j][i]+arr[i][k];
-//                                System.out.println(j +"  "+ answer[j]);
-                            }
-//                        }
-
-                    }
-                }
-            }
-            int answer2 = 0;
-            for(int i = 1; i<= n ; i++){
-                int sum = 0;
-                for(int j =1; j<=n ; j++){
-                    if(m >= arr[i][j]){
+                bfs(i);
+                int sum =0;
+                for(int j=1 ; j<= n ; j++){
+                    if(m>= answer[j]){
                         sum+=map[j];
                     }
 
                 }
-                answer2 = Math.max(sum, answer2);
+                answer3[i]= sum;
+            }
+            int answer2 = 0;
+            for(int i = 1 ;i <= n ; i++){
+
+                answer2 = Math.max(answer2, answer3[i]);
             }
 
             System.out.println(answer2);
+        }
+
+        static void bfs(int start){
+            PriorityQueue<G> pq = new PriorityQueue<>();
+            int [] visited = new int[n+1];
+            answer = new int[n+1];
+
+            for(int i =1; i<= n ; i++){
+                answer[i]= INF;
+            }
+
+            pq.offer(new G(start,start,0));
+            answer[start]=0;
+
+            while(!pq.isEmpty()){
+
+                G tmp = pq.poll();
+                int cur = tmp.end;
+                int cost = tmp.cost;
+                if(visited[cur]==1) continue;
+                visited[cur] = 1;
+
+                for(G now  : maps[cur]){
+
+                    if(visited[now.end]==0 && answer [now.end] > answer[cur]+now.cost ){
+                        answer[now.end] = answer[cur]+now.cost;
+                        pq.offer(new G(now.st, now.end, answer[now.end]));
+                    }
+
+                }
+            }
+
+
+
+
 
         }
     }
