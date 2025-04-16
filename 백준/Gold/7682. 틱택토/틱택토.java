4ad186cbe9cd2acd515e9abc6f0b0ea6640b4156
0u0 <120684605+ugyeong0u0@users.noreply.github.com>
2025-04-16 22:38:40 +0900

    [Gold V] Title: 틱택토, Time: 100 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Gold/7682. 틱택토/틱택토.java
+++ 백준/Gold/7682. 틱택토/틱택토.java
@@ -1,107 +1,79 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-    static char[][] board;
-    static int playerX, playerO, winX, winO;
+	static char[][] board;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String input = "";
-        while (!(input = br.readLine()).equals("end")) {
-            board = new char[3][3];
-            playerX = 0;
-            playerO = 0;
-            winX = 0;
-            winO = 0;
+		StringBuilder sb = new StringBuilder();
+		while(true) {
+			String s = br.readLine();
+			if(s.equals("end")) break;
 
-            makeBoard(input);
-            if (playerO > playerX) {
-                sb.append("invalid").append("\n");
-            } else if (playerX - playerO == 1 || playerX - playerO == 0) {
-                checkBoard();
-                if (playerX + playerO == 9) {
-                    if (winO > 0) {
-                        sb.append("invalid").append("\n");
-                    } else {
-                        sb.append("valid").append("\n");
-                    }
-                } else {
-                    if (winX + winO != 1) {
-                        sb.append("invalid").append("\n");
-                    } else {
-                        if (playerX - playerO == 1 && winO == 1) {
-                            sb.append("invalid").append("\n");
-                        } else if (playerX - playerO == 0 && winX == 1) {
-                            sb.append("invalid").append("\n");
-                        } else {
-                            sb.append("valid").append("\n");
-                        }
-                    }
-                }
-            } else {
-                sb.append("invalid").append("\n");
-            }
-        }
+			int xCnt = 0;
+			int oCnt = 0;
+			board = new char[3][3];
+			for(int i=0; i<3; i++) {
+				for(int j=0; j<3; j++) {
+					board[i][j] = s.charAt(3*i+j);
 
-        System.out.println(sb);
-    }
+					if(board[i][j]=='X') xCnt++;
+					else if(board[i][j]=='O') oCnt++;
+				}
+			}
+			//게임판이 꽉 채워졌을 때
+			//X가 먼저 말을 놓았음으로 O보다 1개 무조건 많아야 한다.
+			if(oCnt+xCnt==9 && xCnt-oCnt==1) {
+				//한 명이 빙고를 완성하면 게임이 끝나기 때문에
+                //둘 다 빙고가 성립될 수 없다.
+				if(Check('X') && Check('O')) sb.append("invalid\n");
+				//말이 꽉 채워진 경우에는 O가 이길 수 없다
+				else if(Check('O')) sb.append("invalid\n");
+				//X가 이긴 경우
+				else sb.append("valid\n");
+			}else{ //게임판이 꽉 채워지지 않은 경우
+				//위와 동일하게 한 명이 빙고가 되면 
+                //끝나야해서 둘 다 빙고인 경우는 없다.
+				if(Check('X') && Check('O')) sb.append("invalid\n");
+				//X가 이겼을 땐, X가 선공이어서 무조건 O보다 하나 많아야 한다.
+				else if(Check('X') && xCnt-oCnt==1) sb.append("valid\n");
+				//O가 이겼을 땐, O가 후공이어서 X와 O의 크기가 같아야만 한다.
+				else if(Check('O') && xCnt==oCnt) sb.append("valid\n");
+				//아무도 이기지 않았는데 말이 다 채워지지 않는 경우는 불가능하다.
+				else sb.append("invalid\n");
+			}
+		}
+		System.out.println(sb.toString());
+	}
+	
+	private static boolean Check(char tiktakto) {
 
-    private static void checkBoard() {
-        for (int i = 0; i < 3; i++) {
-            String row = "";
-            String col = "";
-            for (int j = 0; j < 3; j++) {
-                row += board[i][j];
-                col += board[j][i];
-            }
-            if (row.equals("XXX")) {
-                winX++;
-            }
-            if (col.equals("XXX")) {
-                winX++;
-            }
-            if (row.equals("OOO")) {
-                winO++;
-            }
-            if (col.equals("OOO")) {
-                winO++;
-            }
-        }
+		//가로가 성립할 때
+		for(int i=0; i<3; i++) {
+			int cnt = 0;
+			for(int j=0; j<3; j++) {
+				if(board[i][j]==tiktakto) cnt++;
+			}
+			if(cnt==3) return true;
+		}
+		//세로로 성립할 때
+		for(int j=0; j<3; j++) {
+			int cnt = 0;
+			for(int i=0; i<3; i++) {
+				if(board[i][j]==tiktakto) cnt++;
+			}
+			if(cnt==3) return true;
+		}
+		//대각선으로 성립할 때
+		if(board[0][0]==tiktakto && board[1][1]==tiktakto 
+        				&& board[2][2]==tiktakto) return true;
+		if(board[0][2]==tiktakto && board[1][1]==tiktakto 
+        				&& board[2][0]==tiktakto) return true;
 
-        String x = "";
-        String y = "";
-        for (int i = 0; i < 3; i++) {
-            x += board[i][i];
-            y += board[i][2 - i];
-        }
-        if (x.equals("XXX")) {
-            winX++;
-        }
-        if (y.equals("XXX")) {
-            winX++;
-        }
-        if (x.equals("OOO")) {
-            winO++;
-        }
-        if (y.equals("OOO")) {
-            winO++;
-        }
-    }
-
-    private static void makeBoard(String input) {
-        for (int i = 0; i < input.length(); i++) {
-            char c = input.charAt(i);
-            if (c == 'X') {
-                playerX++;
-            } else if (c == 'O') {
-                playerO++;
-            }
-            board[i / 3][i % 3] = c;
-        }
-    }
-
+		return false;
+	}
 }
